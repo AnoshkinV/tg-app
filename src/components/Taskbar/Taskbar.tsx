31208b181810469b27919089b717a0a9ab4d1fc7
Vov4 <anoshkinvova5554@gmail.com>
2025-02-01 15:49:28 +0200

    simple reward sys

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import './Taskbar.css'
+import { observer } from 'mobx-react-lite'
+import { taskStore } from '../../store/TaskStore'
 
-const Taskbar = () => {
+const Taskbar = observer(() => {
   return (
     <div className='tasks inner-width'>
-        <div className='task-item'>
-            <span>task 1</span><button>join</button>
-            <span>task 1</span><button>join</button>
-            <span>task 1</span><button>join</button>
+      {taskStore.tasks.map(task =>
+        <div className='task-item' key={task.id}>
+          <span>{task.title}</span>
+          {task.status === "notCompleted" && (
+            <button onClick={() => taskStore.updateTaskStatus(task.id, "completed")}>Complete Task</button>
+          )}
+          {task.status === "completed" && (
+            <button onClick={() => taskStore.updateTaskStatus(task.id, "claimed")}>Claim Reward</button>
+          )}
+          {task.status === "claimed" && (
+            <span>Reward Claimed</span>
+          )}
+          
         </div>
+      )}
     </div>
   )
-}
+})
 
 export default Taskbar
